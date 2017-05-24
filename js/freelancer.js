beb83f4eb4f136a4436fa61d6726008423ac1b39
Erica Yarmol-Matusiak <Erica@Ericas-MacBook-Air.local>
2017-05-23 23:16:08 -0400

    first update

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,11 +15,11 @@
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 51
+        offset: 25
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
